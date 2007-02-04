9e76a4477d8afb61fbf9fb18f82d5785d0cce1c7
Fiz Vazquez <vud1@sindominio.net>
2007-02-04 09:11:27 +0000

    "Logo en todas las ventanas"

--- makedeb.sh
+++ makedeb.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 
-version="1.2.0"
+version="1.2.1"
 
 cd ..
 
-cp -rf pytrainer2 pytrainer-$version
+cp -rf trunk pytrainer-$version
 tar -cvzf pytrainer-$version.tar.gz --exclude=*.pyc --exclude=.svn --exclude=DOC --exclude=extensions --exclude=maps pytrainer-$version
 rm -rf pytrainer-$version
 mv pytrainer-$version.tar.gz /home/vud1/debian/pytrainer/
 
-cd pytrainer
+cd trunk
 
 echo "se ha creado /home/vud1/debian/pytrainer/pytrainer-$version.tar.gz"
 
