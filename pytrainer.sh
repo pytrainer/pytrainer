39c1c3833dcd7e9217835b7faa62fc4c8eda2263
David García Granda <dgranda@gmail.com>
2009-12-26 18:46:42 +0000

    Removing wrong start scripts to avoid confusion

--- pytrainer.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-# First arg is log level, e.g. -d for DEBUG
-
-export LD_LIBRARY_PATH=/usr/lib/firefox
-# Building xulrunner path to avoid problems when upgrading
-PATH_XULRUNNER=/usr/lib
-XULRUNNER_VERSION=`ls "$PATH_XULRUNNER" | grep xulrunner`
-export MOZILLA_FIVE_HOME=$PATH_XULRUNNER/$XULRUNNER_VERSION
-python pytrainer.py $*
