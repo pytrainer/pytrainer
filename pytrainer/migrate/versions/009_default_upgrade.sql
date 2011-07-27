a9fc5460aac8d8f9afdc20fc7b1623e7084083d8
Nathan Jones <nathan@ncjones.com>
2011-07-27 12:34:35 +0000

    Fix upgrade script.
    ticket:130

--- pytrainer/migrate/versions/009_default_upgrade.sql
+++ pytrainer/migrate/versions/009_default_upgrade.sql
@@ -1,6 +1,6 @@
 -- lap info added in version 1.9.0
 alter table laps add intensity varchar(7);
-alter table laps add trigger(9);
+alter table laps add laptrigger(9);
 alter table laps add max_speed float;
 alter table laps add avg_hr int;
 alter table laps add max_hr int;
