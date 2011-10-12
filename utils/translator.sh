d67a9d6b7a06eeea1faa03aa5a4e3c783f512323
Nathan Jones <nathan@ncjones.com>
2011-10-12 09:46:17 +0000

    Translator script: add info messages.

--- utils/translator.sh
+++ utils/translator.sh
@@ -7,18 +7,21 @@ if [ $? != 0 ]; then exit ; fi
 
 cd ../
 
-# Extract translatable strings from input files
+echo "Extracting translatable strings from input files"
 xgettext glade/*.glade -o ./messages.pot
 if [ $? != 0 ]; then echo "WARNING: xgettext not found. Please install gettext package"; exit; fi
 find ./ -iname "*.py" -exec xgettext -k_ -j -o ./messages.pot {} \;
 
-# Initializing translations for desired language
+CWD=$(pwd)
+echo "Generated $CWD/messages.pot"
 
 if test "$LANGUAGE" = "$NO_LANGUAGE"; then exit; fi
+
+echo "Initializing translations for desired language"
 msginit -i ./messages.pot -l $LANGUAGE -o ./locale/$LANGUAGE/LC_MESSAGES/pytrainer_$LANGUAGE.po_new
 
 cd ./locale/$LANGUAGE/LC_MESSAGES/
-# Merging old po file with the one generated as a result of current script
+echo "Merging old po file with newly generated one"
 make merge
 
 SOFTWARE=$(zenity --list --text="Select your translation software" --radiolist --column "Pick" --column "Software" TRUE "gtranslator" FALSE "kbabel" FALSE "poedit")
@@ -28,5 +31,5 @@ if [ $? != 0 ]; then exit ; fi
 $SOFTWARE pytrainer_$LANGUAGE.po
 if [ $? != 0 ]; then echo "WARNING: $SOFTWARE not found"; exit ; fi
 
-# Compiling our new message catalog into binary files ready for distribution
+echo "Compiling new message catalog"
 make
