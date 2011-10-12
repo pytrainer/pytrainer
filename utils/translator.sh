e46200837b3a725ba27f9fbb5ec9611b6a054ef5
Nathan Jones <nathan@ncjones.com>
2011-10-12 09:45:57 +0000

    Translator script: add option to generate .pot only.

--- utils/translator.sh
+++ utils/translator.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-SOFTWARE=$(zenity --list --text="Select your translation software" --radiolist --column "Pick" --column "Software" TRUE "gtranslator" FALSE "kbabel" FALSE "poedit")
-if [ $? != 0 ]; then exit ; fi
+NO_LANGUAGE='None (generate .pot)'
 
-LANGUAGE=$(zenity --list --text="Select your language" --radiolist --column "Pick" --column "Language" TRUE "es" FALSE "eu" FALSE "ca" FALSE "fr" FALSE "da" FALSE "de" FALSE "pl" FALSE "no" FALSE "cs" FALSE "ru" FALSE "pl" FALSE "sv" FALSE "gl")
+LANGUAGE=$(zenity --list --text="Select your language" --radiolist --column "Pick" --column "Language" TRUE "$NO_LANGUAGE" FALSE "es" FALSE "eu" FALSE "ca" FALSE "fr" FALSE "da" FALSE "de" FALSE "pl" FALSE "no" FALSE "cs" FALSE "ru" FALSE "pl" FALSE "sv" FALSE "gl")
 if [ $? != 0 ]; then exit ; fi
 
 cd ../
@@ -14,12 +13,16 @@ if [ $? != 0 ]; then echo "WARNING: xgettext not found. Please install gettext p
 find ./ -iname "*.py" -exec xgettext -k_ -j -o ./messages.pot {} \;
 
 # Initializing translations for desired language
+
+if test "$LANGUAGE" = "$NO_LANGUAGE"; then exit; fi
 msginit -i ./messages.pot -l $LANGUAGE -o ./locale/$LANGUAGE/LC_MESSAGES/pytrainer_$LANGUAGE.po_new
-rm ./messages.pot
 
 cd ./locale/$LANGUAGE/LC_MESSAGES/
 # Merging old po file with the one generated as a result of current script
 make merge
+
+SOFTWARE=$(zenity --list --text="Select your translation software" --radiolist --column "Pick" --column "Software" TRUE "gtranslator" FALSE "kbabel" FALSE "poedit")
+if [ $? != 0 ]; then exit ; fi
 
 # Editing our new gettext catalog (.po file)
 $SOFTWARE pytrainer_$LANGUAGE.po
