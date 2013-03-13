a83e24ee58ad958a15987936989b72c6e30eea39
David García Granda <dgranda@gmail.com>
2013-03-13 18:06:17 +0100

    Upgrading version to 1.11.0

--- pytrainer/__init__.py
+++ pytrainer/__init__.py
@@ -1,5 +1,5 @@
-# Based in Django's approach -> http://code.djangoproject.com/svn/django/trunk/django/__init__.py
-VERSION = (1, 10, 1, 'dev')
+# Based on Django's approach -> http://code.djangoproject.com/svn/django/trunk/django/__init__.py
+VERSION = (1, 11, 0, 'dev')
 #VERSION = (1, 11, 0, 'final')
 
 def get_version(version=None):
