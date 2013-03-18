f0b9f4fcc5316a4607f954a8adafb34cdd1824d5
David García Granda <dgranda@gmail.com>
2013-03-18 17:55:59 +0100

    Better import entries readability

--- pytrainer/lib/sqliteUtils.py
+++ pytrainer/lib/sqliteUtils.py
@@ -19,7 +19,9 @@
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 import logging
-import sys, traceback, commands
+import sys
+import traceback
+import commands
 import datetime
 try:
     from sqlite3 import dbapi2 as sqlite
