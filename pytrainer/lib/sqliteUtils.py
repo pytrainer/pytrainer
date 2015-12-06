6e1eb4722552a1c81156f9a3116c7b04997cff22
Arto Jantunen <viiru@iki.fi>
2015-12-06 10:57:34 +0200

    sqliteUtils: set sqlalchemy url in __init__

--- pytrainer/lib/sqliteUtils.py
+++ pytrainer/lib/sqliteUtils.py
@@ -33,9 +33,10 @@ class Sql:
         self.db = None
         confdir = configuration.confdir
         self.ddbb = "%s/pytrainer.ddbb" %confdir
+        self.url = "sqlite:///" + self.ddbb
         
     def get_connection_url(self):
-        return "sqlite:///" + self.ddbb
+        return self.url
     
     def connect(self):
         #si devolvemos 1 ha ido todo con exito
