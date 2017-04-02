c0da1ba62dd5f8af111ed73f4a03ddd91a48d6a8
Arto Jantunen <viiru@iki.fi>
2017-04-02 12:23:40 +0300

    Move gettext initialization to a separate library function

---
+++ pytrainer/lib/localization.py
@@ -0,0 +1,28 @@
+#Copyright (C) Fiz Vazquez vud1@sindominio.net
+#Copyright (C) Arto Jantunen <viiru@iki.fi>
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import gettext
+import pygtk
+pygtk.require('2.0')
+import gtk.glade
+
+def initialize_gettext(gettext_path):
+    gettext.bindtextdomain("pytrainer", gettext_path)
+    gtk.glade.bindtextdomain("pytrainer", gettext_path)
+    gtk.glade.textdomain("pytrainer")
+    gettext.textdomain("pytrainer")
+    gettext.install("pytrainer", gettext_path, unicode=1)
