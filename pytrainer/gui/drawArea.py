2dadeaf708bbfc00100833309de53065432e115e
Arto Jantunen <viiru@iki.fi>
2017-07-23 10:14:00 +0300

    Note that matplotlib is always using the GTKAgg backend
    In reality as far as I can tell this was the case due to the imports, but
    this single line requested the deprecated GTK backend instead.

--- pytrainer/gui/drawArea.py
+++ pytrainer/gui/drawArea.py
@@ -17,7 +17,7 @@
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 import matplotlib
-matplotlib.use('GTK')
+matplotlib.use('GTKAgg')
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvasGTK
 from matplotlib.backends.backend_gtkagg import NavigationToolbar2GTKAgg as NavigationToolbar
