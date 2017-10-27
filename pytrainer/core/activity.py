pytrainer/pytrainer

commit 8c0328c8fb04047142a5e995932da6d46d01c958

Add ActivityService.remove_activity_from_db method to replace Record.removeRecord

pytrainer/core/activity.py
@@ -50,6 +50,14 @@ class Lap(DeclarativeBase):
     start_lat = Column(Float)
     start_lon = Column(Float)
 
+class ActivityServiceException(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 class ActivityService(object):
     '''
     Class maintains a pool of activities
@@ -104,6 +112,19 @@ class ActivityService(object):
         logging.debug("ActivityPool queue length: %d" % len(self.pool_queue))
         logging.debug("ActivityPool queue: %s" % str(self.pool_queue))
         return self.pool[sid]
+
+    def remove_activity_from_db(self, activity):
+        """Delete a stored Activity."""
+
+        if not activity.id:
+            raise ActivityServiceException("Cannot remove activity which has not been stored: '{0}'.".format(activity.name))
+        try:
+            self.remove_activity_from_cache(activity.id)
+            self.pytrainer_main.ddbb.session.delete(activity)
+            self.pytrainer_main.ddbb.session.commit()
+        except InvalidRequestError:
+             raise ActivityServiceException("Activity id %s not found" % activity.id)
+        logging.debug("Deleted activity: %s", activity.title)
 
 class Activity(DeclarativeBase):
     '''

pytrainer/main.py
@@ -480,7 +480,8 @@ class pyTrainer:
     def removeRecord(self, id_record, confirm = False, view=None):
         logging.debug('>>')
         if confirm:
-             self.record.removeRecord(id_record)
+             activity = self.activitypool.get_activity(id_record)
+             self.activitypool.remove_activity_from_db(activity)
         else:
              msg = _("Delete this database entry?")
              params = [id_record,True]

pytrainer/record.py
@@ -70,17 +70,6 @@ class Record:
                 self.pytrainer_main.refreshMainSportList()
 		logging.debug('<<')
 
-	def removeRecord(self,id_record):
-		logging.debug('>>')
-		record = self.pytrainer_main.ddbb.delete("records", "id_record=\"%s\"" %id_record)
-		laps = self.pytrainer_main.ddbb.delete("laps", "record=\"%s\"" %id_record)
-		logging.debug('removed record '+str(id_record)+' (and associated laps) from DB')
-		gpxfile = self.pytrainer_main.profile.gpxdir+"/%d.gpx"%int(id_record)
-		if os.path.isfile(gpxfile):
-			os.remove(gpxfile)
-			logging.debug('removed gpxfile '+gpxfile)
-		logging.debug('<<')
-
 	def pace_to_float(self, value):
 		'''Take a mm:ss or mm.ss and return float'''
 		try:

pytrainer/test/core/test_activity.py
@@ -18,6 +18,7 @@ import unittest
 import datetime
 from mock import Mock
 from dateutil.tz import tzoffset
+from sqlalchemy.orm.exc import NoResultFound
 
 from pytrainer.lib.ddbb import DDBB, DeclarativeBase
 from pytrainer.profile import Profile
@@ -126,3 +127,12 @@ class ActivityTest(unittest.TestCase):
     def test_activity_service_null(self):
         none_activity = self.service.get_activity(None)
         self.assertIsNone(none_activity.id)
+
+    def test_activity_remove(self):
+        self.service.remove_activity_from_db(self.activity)
+        try:
+            self.service.get_activity(1)
+        except NoResultFound:
+            pass
+        else:
+            self.fail()

commit 003791a8e9c39f4e08bea9ffdd57426263a236b2

Remove the gpx file when deleting an Activity
This fixes a bug introduced in 8c0328c8fb04.

pytrainer/core/activity.py
@@ -17,7 +17,7 @@
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 import logging
-import os
+import os, os.path
 import dateutil.parser
 from dateutil.tz import tzlocal
 
@@ -122,6 +122,8 @@ class ActivityService(object):
             self.remove_activity_from_cache(activity.id)
             self.pytrainer_main.ddbb.session.delete(activity)
             self.pytrainer_main.ddbb.session.commit()
+            if activity.gpx_file and os.path.isfile(activity.gpx_file):
+                os.remove(activity.gpx_file)
         except InvalidRequestError:
              raise ActivityServiceException("Activity id %s not found" % activity.id)
         logging.debug("Deleted activity: %s", activity.title)