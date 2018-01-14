b532f20239089a4be3c09c0b5be2fa7fa8c014fc
Arto Jantunen <viiru@iki.fi>
2018-01-14 12:41:58 +0200

    Convert actualize_monthview to use Activity and the new helper method

--- pytrainer/main.py
+++ pytrainer/main.py
@@ -285,7 +285,7 @@ class pyTrainer:
              sport_id = self.record.getSportId(sport)
              record_list = self.record.getrecordPeriod(date_range, sport_id)
              nameMonth, daysInMonth = getNameMonth(date_selected)
-             self.windowmain.actualize_monthview(record_list, nameMonth)
+             self.windowmain.actualize_monthview(record_list, nameMonth, date_range)
              self.windowmain.actualize_monthgraph(record_list, daysInMonth)
         elif view=="year":
              logging.debug('year view')
