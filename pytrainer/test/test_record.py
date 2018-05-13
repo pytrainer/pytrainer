pytrainer/pytrainer

commit f7ed8aa344ed2946c2b811ef4a5d2ebb9814f8ed

Fix date handling bug introduced when converting _formatRecordNew to sqlalchemy

pytrainer/test/test_record.py
@@ -17,6 +17,7 @@
 import unittest
 import mock
 from datetime import datetime
+from dateutil.tz import tzoffset
 from pytrainer.lib.ddbb import DDBB
 from pytrainer.core.sport import SportService
 from pytrainer.record import Record
@@ -54,7 +55,7 @@ class RecordTest(unittest.TestCase):
         self.summary['rcd_maxbeats'] = 120
         self.summary['rcd_upositive'] = 553.1
         self.summary['rcd_unegative'] = 564.1
-        self.summary['date_time_local'] = datetime(2016, 7, 24, 9, 58, 23)
+        self.summary['date_time_local'] = u'2016-07-24 12:58:23+03:00'
         self.summary['date_time_utc'] = u'2016-07-24T09:58:23Z'
         self.laps = [{'distance': 46181.9,
                       'lap_number': 0,
@@ -76,7 +77,8 @@ class RecordTest(unittest.TestCase):
         self.assertEquals(activity.upositive, 553.1)
         self.assertEquals(activity.beats, 115.0)
         self.assertEquals(activity.maxbeats, 120)
-        self.assertEquals(activity.date_time, datetime(2016, 7, 24, 9, 58, 23))
+        self.assertEquals(activity.date_time, datetime(2016, 7, 24, 12, 58, 23,
+                                                       tzinfo=tzoffset(None, 10800)))
         self.assertEquals(activity.date_time_utc, u'2016-07-24T09:58:23Z')
         self.assertEquals(activity.sport, self.record._sport_service.get_sport_by_name(u"Run"))
         self.assertEquals(activity.title, u'test 1')

pytrainer/record.py
@@ -20,6 +20,7 @@
 import os
 import shutil
 import logging
+import datetime
 
 from gui.windowrecord import WindowRecord
 from gui.dialogselecttrack import DialogSelectTrack
@@ -119,14 +120,15 @@ class Record:
         record.average = self.parseFloatRecord(list_options["rcd_average"])
         record.calories = self.parseFloatRecord(list_options["rcd_calories"])
         record.comments = unicode(list_options["rcd_comments"])
-        record.date = list_options["date_time_local"].date()
+        record.date = datetime.datetime.strptime(list_options["date_time_utc"],
+                                                 "%Y-%m-%dT%H:%M:%SZ").date()
         record.unegative = self.parseFloatRecord(list_options["rcd_unegative"])
         record.upositive = self.parseFloatRecord(list_options["rcd_upositive"])
         record.maxspeed = self.parseFloatRecord(list_options["rcd_maxvel"])
         record.maxpace = self.pace_to_float(list_options["rcd_maxpace"])
         record.date_time_utc = list_options["date_time_utc"]
         record.duration = time2second(list_options["rcd_time"])
-        record.date_time_local = list_options["date_time_local"].strftime("%Y-%m-%d %H:%M:%S%z")
+        record.date_time_local = list_options["date_time_local"]
         record.sport = sport
         return record
 

commit 7ee655ef4aeff1c5a48b391f1f4bc5547c149f6a

Add a test to trigger the timezone date bug
This results from using date_time_utc to fill in Activity.date. Bug
introduced in commit f7ed8aa344ed and reported by Håkan Jerning.

pytrainer/test/test_record.py
@@ -16,7 +16,7 @@
 
 import unittest
 import mock
-from datetime import datetime
+from datetime import datetime, date
 from dateutil.tz import tzoffset
 from pytrainer.lib.ddbb import DDBB
 from pytrainer.core.sport import SportService
@@ -119,3 +119,10 @@ list_options['date_time_local'], also test that code path"""
     def test_getLastRecordDateString(self):
         self.record.insertRecord(self.summary)
         self.assertEqual(self.record.getLastRecordDateString(), self.summary['rcd_date'])
+
+    def test_record_midnight_date_bug(self):
+        self.summary['date_time_local'] = u'2016-07-24 0:00:00+03:00'
+        self.summary['date_time_utc'] = u'2016-07-23T21:00:00Z'
+        newid = self.record.insertRecord(self.summary)
+        activity = self.main.activitypool.get_activity(newid)
+        self.assertEqual(activity.date, date(2016, 7, 24))