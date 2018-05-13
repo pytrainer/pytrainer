pytrainer/pytrainer

commit af0dd1beef4a315719813c9331d445bb705a5519

Convert the listview functionality to sqlalchemy

pytrainer/gui/windowmain.py
@@ -1181,10 +1181,6 @@ class Main(SimpleBuilderApp):
 
     def actualize_listview(self,record_list):
         logging.debug(">>")
-        #recod list tiene:
-        #date,distance,average,title,sports.name,id_record,time,beats,caloriesi
-        #Laas columnas son:
-        #column_names=[_("id"),_("Title"),_("Date"),_("Distance"),_("Sport"),_("Time"),_("Beats"),_("Average"),("Calories")]
         store = Gtk.ListStore(
             GObject.TYPE_INT,
             GObject.TYPE_STRING,
@@ -1198,35 +1194,35 @@ class Main(SimpleBuilderApp):
             object)
         for i in record_list:
             try:
-                hour,min,sec = second2time(int(i[6]))
+                hour,min,sec = date.second2time(i.duration)
             except  (ValueError, TypeError):
                 hour,min,sec = (0,0,0)
             _time = "%2d:%02d:%02d" %(hour,min,sec)
             try:
-                _id = int(i[5])
+                _id = i.id
             except (ValueError, TypeError) as e:
                 logging.debug("Unable to determine id for record: %s" % str(i))
                 logging.debug(str(e))
                 continue
-            _title = i[3]
-            _date = str(i[0])
+            _title = i.title
+            _date = i.date
             try:
-                _distance = float(i[1])
+                _distance = float(i.distance)
             except (ValueError, TypeError):
                 _distance = 0
-            _sport = i[4]
+            _sport = i.sport.name
             try:
-                _average = float(i[2])
+                _average = i.average
             except (ValueError, TypeError):
                 _average = 0
             try:
-                _calories = int(i[8])
+                _calories = i.calories
             except (ValueError, TypeError):
                 _calories = 0
             try:
-                _beats = round(float(i[7]))
+                _beats = round(i.beats)
             except (ValueError, TypeError) as e:
-                logging.debug("Unable to parse beats for %s" % str(i[7]) )
+                logging.debug("Unable to parse beats for %s" % str(i.beats) )
                 logging.debug(str(e))
                 _beats = 0.0
 

pytrainer/lib/listview.py
@@ -1,6 +1,8 @@
 import datetime
+from sqlalchemy import and_
 from pytrainer.lib.localization import gtk_str
 from pytrainer.lib.uc import UC
+from pytrainer.core.activity import Activity
 
 UC_LISTDISTANCE = {False : [[_('All Distances'), [0.0,999999.9]],
                             ['<1 km', [0.0, 1.0]],
@@ -62,51 +64,35 @@ class ListSearch(object):
         self.setup_lsa_past()
         self.setup_lsa_duration()
         self.setup_lsa_distance()
-        
+
     def get_condition(self):
-        """ Assembles sqlite condition """
-        _search = ""
-        _add_and = False
-        if self.title != "":
-            _search = "title like '%" +self.title + "%'"
-            _add_and = True
-        if self.sport > 0:
-            _sport = self.listSport[self.sport-1].id
-            _here = "sport=%s" % _sport
-            if _add_and:
-                _search += " and " + _here
-            else:
-                _search = _here
-            _add_and = True
+        """ Assembles an sqlalchemy query object """
+        _andlist = []
+        if self.title:
+            _andlist.append(Activity.like('%' + self.title + '%'))
+        if self.sport:
+            _andlist.append(Activity.sport_id == self.sport)
         if self.listPast[self.past][1]:
-            _delta = datetime.timedelta(days=self.listPast[self.past][1] )
+            _delta = datetime.timedelta(days=self.listPast[self.past][1])
             _date = datetime.datetime.today() + _delta
-            _here = "date>'" + _date.isoformat() + "'"
-            if _add_and:
-                _search += " and " + _here
-            else:
-                _search = _here
-            _add_and = True
+            _andlist.append(Activity.date > _date)
         if self.listDuration[self.duration][1]:
             _dur_min = int(self.listDuration[self.duration][1][0])
             _dur_max = int(self.listDuration[self.duration][1][1])
-            _here = "(duration between %s and %s)" % (_dur_min, _dur_max)
-            if _add_and:
-                _search += " and " + _here
-            else:
-                _search = _here
-            _add_and = True                 
+            _andlist.append(Activity.duration.between(_dur_min, _dur_max))
         if self.listDistance[self.distance][1]:
             _dis_min = int(self.listDistance[self.distance][1][0])
             _dis_max = int(self.listDistance[self.distance][1][1])
-            _here = "(distance between %s and %s)" % (_dis_min, _dis_max)
-            if _add_and:
-                _search += " and " + _here
+            _andlist.append(Activity.distance.between(_dis_min, _dis_max))
+        ret = None
+        first = True
+        for expr in _andlist:
+            if first:
+                ret = expr
+                first = False
             else:
-                _search = _here
-            _add_and = True                                             
-        #print _search
-        return _search
+                ret = and_(ret, expr)
+        return ret
 
     """    
     def get_listDistance(self):

pytrainer/record.py
@@ -358,21 +358,12 @@ class Record:
         return self.pytrainer_main.ddbb.select("records,sports", "date_time_local,title,sports.name,distance,duration,average,maxspeed,pace,maxpace,beats,maxbeats,calories,upositive,unegative,comments",
     "sports.id_sports = records.sport","order by date_time_local asc")
 
-    def getAllRecordList(self):
-        logging.debug('--')
-        return self.pytrainer_main.ddbb.select("records,sports",
-                "date,distance,average,title,sports.name,id_record,duration,beats,calories",
-                "sports.id_sports = records.sport order by date desc")
-
-    def getRecordListByCondition(self,condition):
+    def getRecordListByCondition(self, condition):
         logging.debug('>>')
         if condition is None:
-            return self.getAllRecordList()
+            return self.pytrainer_main.ddbb.session.query(Activity).order_by(Activity.date)
         else:
-            logging.debug("condition: %s" % condition)
-            return self.pytrainer_main.ddbb.select("records,sports",
-                    "date,distance,average,title,sports.name,id_record,duration,beats,calories",
-                    "sports.id_sports = records.sport and %s order by date desc" %condition)
+            return self.pytrainer_main.ddbb.session.query(Activity).filter(condition).order_by(Activity.date)
 
     def getRecordDayList(self, date, sport=None):
         logging.debug('>>')

commit 7bfe016aadea30b2929f5b9f8a1bed13f67785c9

Fix import difference in listview branches
Bug introduced in af0dd1beef4a3.

pytrainer/gui/windowmain.py
@@ -1194,7 +1194,7 @@ class Main(SimpleBuilderApp):
             object)
         for i in record_list:
             try:
-                hour,min,sec = date.second2time(i.duration)
+                hour,min,sec = second2time(i.duration)
             except  (ValueError, TypeError):
                 hour,min,sec = (0,0,0)
             _time = "%2d:%02d:%02d" %(hour,min,sec)