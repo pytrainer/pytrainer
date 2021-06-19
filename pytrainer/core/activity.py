pytrainer/pytrainer

commit 8ca54a97a9f6dab8ae0f3b30591af2fce36b45e6

Replace Activity.pace_from_float with uc.float2pace
Also fixing the previous obviosly invalid test results. 3 minutes and 86
seconds, indeed..

pytrainer/core/activity.py
@@ -26,7 +26,7 @@ from pytrainer.lib.gpx import Gpx
 from pytrainer.lib.graphdata import GraphData
 from pytrainer.lib.unitsconversor import *
 from pytrainer.environment import Environment
-from pytrainer.lib.uc import UC
+from pytrainer.lib import uc
 
 class ActivityService(object):
     '''
@@ -135,7 +135,7 @@ class Activity:
     def __init__(self, pytrainer_main=None, id=None):
         logging.debug(">>")
         self.environment = Environment()
-        self.uc = UC()
+        self.uc = uc.UC()
         self.id = id
         #It is an error to try to initialise with no id
         if self.id is None:
@@ -656,14 +656,14 @@ tracks (%s)
                 return self.maxspeed
         elif param == 'maxpace':
             if self.uc.us:
-                return self.pace_from_float(pacekm2miles(self.maxpace))
+                return uc.float2pace(pacekm2miles(self.maxpace))
             else:
-                return self.pace_from_float(self.maxpace)
+                return uc.float2pace(self.maxpace)
         elif param == 'pace':
             if self.uc.us:
-                return self.pace_from_float(pacekm2miles(self.pace))
+                return uc.float2pace(pacekm2miles(self.pace))
             else:
-                return self.pace_from_float(self.pace)
+                return uc.float2pace(self.pace)
         elif param == 'calories':
             return self.calories
         elif param == 'time':
@@ -732,14 +732,3 @@ tracks (%s)
         except ValueError:
             value = None
         return value
-
-    def pace_from_float(self, value):
-        '''Helper to generate mm:ss from float representation mm.ss (or mm,ss?)'''
-        #Check that value supplied is a float
-        if not value:
-            return ""
-        try:
-            _value = "%0.2f" % float(value)
-        except ValueError:
-            _value = str(value)
-        return _value.replace('.', ':')

pytrainer/test/core/test_activity.py
@@ -80,8 +80,8 @@ class ActivityTest(unittest.TestCase):
         self.assertEquals(self.activity.get_value_f('maxspeed', "%0.2f"), '44.67')
         self.assertEquals(self.activity.get_value_f('time', '%s'), '2:03:46')
         self.assertEquals(self.activity.get_value_f('calories', "%0.0f"), '1462')
-        self.assertEquals(self.activity.get_value_f('pace', "%s"), '2:40')
-        self.assertEquals(self.activity.get_value_f('maxpace', "%s"), '1:20')
+        self.assertEquals(self.activity.get_value_f('pace', "%s"), '2:24')
+        self.assertEquals(self.activity.get_value_f('maxpace', "%s"), '1:12')
         self.assertEquals(self.activity.get_value_f('upositive', "%0.2f"), '553.06')
         self.assertEquals(self.activity.get_value_f('unegative', "%0.2f"), '564.08')
 
@@ -92,8 +92,8 @@ class ActivityTest(unittest.TestCase):
         self.assertEquals(self.activity.get_value_f('maxspeed', "%0.2f"), '27.76')
         self.assertEquals(self.activity.get_value_f('time', '%s'), '2:03:46')
         self.assertEquals(self.activity.get_value_f('calories', "%0.0f"), '1462')
-        self.assertEquals(self.activity.get_value_f('pace', "%s"), '3:86')
-        self.assertEquals(self.activity.get_value_f('maxpace', "%s"), '1:93')
+        self.assertEquals(self.activity.get_value_f('pace', "%s"), '3:52')
+        self.assertEquals(self.activity.get_value_f('maxpace', "%s"), '1:56')
         self.assertEquals(self.activity.get_value_f('upositive', "%0.2f"), '1814.50')
         self.assertEquals(self.activity.get_value_f('unegative', "%0.2f"), '1850.66')
 

commit 03662550555031b06e935b0ad63fb72aa48962b6

Fix activity pace conversion
Commit 8ca54a97a9f6 introduced wrong pace conversion of both metric and imperial
units, before that commit just imperial conversion was faulty.

The activity test case is corrected accordingly:
The float representation of 2.4 in the database means 2 minutes and 40 seconds
per kilometer, which correctly converted is 4:17 minutes per mile.

pytrainer/core/activity.py
@@ -678,9 +678,9 @@ tracks (%s)
         elif param == 'maxspeed':
             return self.uc.speed(self.maxspeed)
         elif param == 'maxpace':
-            return uc.float2pace(self.uc.pace(self.maxpace))
+            return uc.float2pace(self.uc.pace(uc.pace_from_db(self.maxpace)))
         elif param == 'pace':
-            return uc.float2pace(self.uc.pace(self.pace))
+            return uc.float2pace(self.uc.pace(uc.pace_from_db(self.pace)))
         elif param == 'calories':
             return self.calories
         elif param == 'time':

pytrainer/lib/uc.py
@@ -42,6 +42,13 @@ def float2pace(pace_flt):
     _pace_str = "%d:%02d" % (_p_int, _p_frc)  
     return _pace_str
 
+def pace_from_db(pace_db_float):
+    """Helper function to generate real float from the mm.ss float representation in the database"""
+    if not pace_db_float:
+        return None
+    _pace_str = '{:.2f}'.format(pace_db_float).replace('.', ':')
+    return pace2float(_pace_str)
+
 class UC(Singleton):
     """ 
     When instantiated first time us is assigned to False.

pytrainer/test/core/test_activity.py
@@ -120,8 +120,8 @@ class ActivityTest(unittest.TestCase):
         self.assertEqual(self.activity.get_value_f('maxspeed', "%0.2f"), '44.67')
         self.assertEqual(self.activity.get_value_f('time', '%s'), '2:03:46')
         self.assertEqual(self.activity.get_value_f('calories', "%0.0f"), '1462')
-        self.assertEqual(self.activity.get_value_f('pace', "%s"), '2:24')
-        self.assertEqual(self.activity.get_value_f('maxpace', "%s"), '1:12')
+        self.assertEqual(self.activity.get_value_f('pace', "%s"), '2:40')
+        self.assertEqual(self.activity.get_value_f('maxpace', "%s"), '1:20')
         self.assertEqual(self.activity.get_value_f('upositive', "%0.2f"), '553.06')
         self.assertEqual(self.activity.get_value_f('unegative', "%0.2f"), '564.08')
 
@@ -132,8 +132,8 @@ class ActivityTest(unittest.TestCase):
         self.assertEqual(self.activity.get_value_f('maxspeed', "%0.2f"), '27.76')
         self.assertEqual(self.activity.get_value_f('time', '%s'), '2:03:46')
         self.assertEqual(self.activity.get_value_f('calories', "%0.0f"), '1462')
-        self.assertEqual(self.activity.get_value_f('pace', "%s"), '3:52')
-        self.assertEqual(self.activity.get_value_f('maxpace', "%s"), '1:56')
+        self.assertEqual(self.activity.get_value_f('pace', "%s"), '4:17')
+        self.assertEqual(self.activity.get_value_f('maxpace', "%s"), '2:09')
         self.assertEqual(self.activity.get_value_f('upositive', "%0.2f"), '1814.50')
         self.assertEqual(self.activity.get_value_f('unegative', "%0.2f"), '1850.66')
 

pytrainer/test/lib/test_uc.py
@@ -31,6 +31,9 @@ class UCUtilTest(unittest.TestCase):
     def test_uc_float2pace(self):
         self.assertEqual('4:06', float2pace(4.1))
 
+    def test_uc_pace_from_db(self):
+        self.assertEqual(4.1, pace_from_db(4.06))
+
 class UCTest(unittest.TestCase):
 
     def setUp(self):