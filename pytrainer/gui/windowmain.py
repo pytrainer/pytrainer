pytrainer/pytrainer

commit 6e28b5c160ee5fd08978303bc5e98d69279e6fc5

Just show one decimal of athlete body fat

pytrainer/gui/windowmain.py
@@ -118,7 +118,7 @@ class Main(SimpleBuilderApp):
         columns=[   {'name':_("id"), 'visible':False},
                     {'name':_("Date")},
                     {'name':_("Weight"), 'xalign':1.0, 'quantity':'weight', 'format_float':'%.1f'},
-                    {'name':_("Body Fat %"), 'xalign':1.0},
+                    {'name':_("Body Fat %"), 'xalign':1.0, 'quantity':'bodyfat', 'format_float':'%.1f'},
                     {'name':_("Resting HR"), 'xalign':1.0},
                     {'name':_("Max HR"), 'xalign':1.0}
                 ]

commit ed52c1ed031077a3a5f12cf4f172b6871a3ced94

Just show one decimal of athlete body fat for US units
Commit 6e28b5c160ee introduced an exception with US units since format_float
demands an existing quantity for conversion.

pytrainer/gui/windowmain.py
@@ -120,7 +120,7 @@ class Main(SimpleBuilderApp):
         columns=[   {'name':_("id"), 'visible':False},
                     {'name':_("Date")},
                     {'name':_("Weight"), 'xalign':1.0, 'quantity':'weight', 'format_float':'%.1f'},
-                    {'name':_("Body Fat %"), 'xalign':1.0, 'quantity':'bodyfat', 'format_float':'%.1f'},
+                    {'name':_("Body Fat %"), 'xalign':1.0, 'quantity':'none', 'format_float':'%.1f'},
                     {'name':_("Resting HR"), 'xalign':1.0},
                     {'name':_("Max HR"), 'xalign':1.0}
                 ]

pytrainer/lib/uc.py
@@ -63,7 +63,7 @@ class UC(Singleton):
 
     """ Conversion factors from metric to imperial, units as in uc_units """
     uc_factors = {'distance' : 0.621371192, 'speed': 0.621371192, 'pace':1.609344,
-                  'height': 3.2808399, 'weight': 2.204624}
+                  'height': 3.2808399, 'weight': 2.204624, 'none': 1}
 
     def __init__(self):
         if not hasattr(self, 'us'):