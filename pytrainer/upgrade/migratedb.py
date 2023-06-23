65af8565cb26ae041f4b5e21a8711f34db113fd3
Arto Jantunen <viiru@iki.fi>
2023-06-23 17:50:16 +0300

    Stop using sqlalchemy-migrate
    Fake the migration support instead, and just error out if a migration is
    actually needed.

--- pytrainer/upgrade/migratedb.py
+++ pytrainer/upgrade/migratedb.py
@@ -1,86 +1,102 @@
 # -*- coding: utf-8 -*-
 
-#Copyright (C) Nathan Jones ncjones@users.sourceforge.net
+# Copyright (C) Nathan Jones ncjones@users.sourceforge.net
+# Copyright (C) Arto Jantunen <viiru@iki.fi>
 
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-from migrate.versioning.api import db_version, upgrade, version, version_control
-from sqlalchemy.exc import NoSuchTableError
-# sqlalchemy-migrate 0.6.1 broke backwards compatibility
-# so we need to try importing exceptions from one of two packages
-try:
-    from migrate.exceptions import DatabaseNotControlledError
-except:
-    from migrate.versioning.exceptions import DatabaseNotControlledError
-from sqlalchemy.schema import MetaData
-import logging
 import sqlalchemy
-import sys
+from sqlalchemy.exc import NoSuchTableError, OperationalError
+from sqlalchemy.sql.expression import func
+from sqlalchemy.schema import MetaData
 
-class MigratableDb(object):
-    """Object bridge for sqlalchemy-migrate API functions."""
-    
-    def __init__(self, repository_path, db_url):
+from pytrainer.lib.ddbb import DeclarativeBase
+
+
+class MigrateVersion(DeclarativeBase):
+    __tablename__ = 'migrate_version'
+
+    repository_id = sqlalchemy.Column(sqlalchemy.String(250), primary_key=True)
+    repository_path = sqlalchemy.Column(sqlalchemy.Text)
+    version = sqlalchemy.Column(sqlalchemy.Integer)
+
+
+class MigratableDb:
+    """Tool to check and return database version."""
+
+    def __init__(self, ddbb):
         """Create a migratable DB.
-        
+
         Arguments:
         repository_path -- The path to the migrate repository, relative to the
             pypath.
         db_url -- the connection URL string for the DB.
         """
-        self._repository_path = repository_path
-        self._db_url = db_url
-        
+        self.ddbb = ddbb
+
     def is_empty(self):
         """Check if the DB schema is empty.
-        
+
         An empty schema indicates a new uninitialised database."""
         metadata = MetaData()
-        metadata.bind = sqlalchemy.create_engine(self._db_url)
+        metadata.bind = self.ddbb.engine
         metadata.reflect()
         tables = metadata.tables
         return not tables
-        
+
     def is_versioned(self):
         """ Check if the DB has been initiaized with version metadata."""
         try:
             self.get_version()
-        except DatabaseNotControlledError:
+        except OperationalError:
             return False
         except NoSuchTableError:
-            # prior to 0.6.0, sqlalchemy-migrate did not handle querying
-            # against unversioned databases.
             return False
         return True
-        
+
     def get_version(self):
         """Get the current version of the versioned DB.
-        
-        Raises DatabaseNotControlledError if the DB is not initialized."""
-        return db_version(self._db_url, self._repository_path)
-    
+
+        Raises OperationError if the DB is not initialized."""
+        latest = self.ddbb.session.query(func.max(MigrateVersion.version)).one()
+        return latest[0]
+
     def get_upgrade_version(self):
         """Get the latest version available in upgrade repository."""
-        return version(self._repository_path)
-    
+        return 15
+
     def version(self, initial_version):
         """Initialize the database with migrate metadata.
-        
+
         Raises DatabaseAlreadyControlledError if the DB is already initialized."""
-        version_control(self._db_url, self._repository_path, initial_version)
+        self.ddbb.session.add(
+            MigrateVersion(
+                repository_id='pytrainer',
+                repository_path='/usr/lib/python3/site-packages/pytrainer/upgrade',
+                version=15,
+                )
+            )
+        self.ddbb.session.commit()
 
     def upgrade(self):
         """Run all available upgrade scripts for the repository."""
-        upgrade(self._db_url, self._repository_path)
+        from pytrainer.gui.dialogs import warning_dialog
+        current_version = self.get_version()
+        warning_dialog(
+            title="Database migration not supported",
+            text=f"Migrating from database version {current_version} not supported. \
+Please use pytrainer version 2.1.0 to migrate the database.",
+        )
+        exit(1)
