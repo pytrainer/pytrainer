687aea3808d27072cd1075e2b0dd98336b3bde9b
Håkan Jerning <git@jerning.se>
2023-10-04 20:56:13 +0200

    Remove dead code
    This haven't been used since commit 719a78854f64.

--- pytrainer/monthgraph.py
+++ pytrainer/monthgraph.py
@@ -16,9 +16,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-import dateutil
-import datetime
-from .timegraph import TimeGraph
+from pytrainer.timegraph import TimeGraph
 
 class MonthGraph(TimeGraph):
 
@@ -35,61 +33,6 @@ class MonthGraph(TimeGraph):
         self.combovalue = combovalue
         self.combovalue2 = combovalue2
         self.KEY_FORMAT = "%d"
-        
+
     def drawgraph(self,values, daysInMonth):
         TimeGraph.drawgraph(self, values, x_func=lambda x: list([u'%02d' % d for d in range(1,daysInMonth+1)]))
-
-    def get_values2(self,values,value_selected,daysInMonth):
-        #hacemos una relacion entre el value_selected y los values / we make a relation between value_selected and the values
-        conv = {
-            0: 1, #value 0 es kilometros (1)
-            1: 2, #value 1 es tiempo (2)
-            2: 3, #value 2 es pulsaciones(3)
-            3: 5, #value 3 es media(5)
-            4: 6 #value 4 es calorias(6)
-            }
-        list_values = {}
-        list_average = {}
-        for i in range(1,daysInMonth+1):
-            list_values[i]=0
-            list_average[i]=0
-
-        value_sel = conv[value_selected]
-        for value in values:
-            #si la opcion es tiempo lo pasamos a horas / if the option is time we passed it to hours
-            if (value_sel == 2):
-                graph_value = self.getFloatValue(value[value_sel])/3600
-            else:
-                graph_value = self.getFloatValue(value[value_sel])
-                 
-            #TODO Sort date handling...
-            date = value[0]
-            try:
-                year,month,day = date.year, date.month, date.day
-            except AttributeError:
-                year,month,day = date.split("-")
-
-            #si es una opcion de suma de absolutos / if it is an option of sum of absolute
-            if ((value_selected == 0) or (value_selected==1) or (value_selected==4)):
-                list_values[int(day)] += graph_value
-            #si se trata de calcular medias / if one is to calculate averages:
-            else:
-                if graph_value is not None and graph_value != 0:
-                    list_values[int(day)] += graph_value
-                    list_average[int(day)] += 1
-
-        xunits = []
-        yunits = []
-        for i in range (1,daysInMonth+1):
-            xunits.append(i)
-            yunits.append(float(0))
-    
-        for value in list_values:
-            if ((value_selected == 0) or (value_selected==1) or (value_selected==4)):
-                yunits[value-1] = list_values[value]
-            else:
-                if list_average[value]>0:
-                    yunits[value-1] = list_values[value]/list_average[value]
-
-        return xunits,yunits
-    
--- pytrainer/yeargraph.py
+++ pytrainer/yeargraph.py
@@ -16,10 +16,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-import calendar
-import datetime
 from pytrainer.timegraph import TimeGraph
-from pytrainer.lib.localization import locale_str
 
 class YearGraph(TimeGraph):
 
@@ -39,94 +36,3 @@ class YearGraph(TimeGraph):
 
     def drawgraph(self,values):
         TimeGraph.drawgraph(self, values, x_func=lambda x: ["%02d" % m for m in range(1,13)])
-
-    def drawgraph2(self,values):
-        
-        value_selected = self.combovalue.get_active()
-        value_selected2 = self.combovalue2.get_active()
-        if value_selected < 0:
-            self.combovalue.set_active(0)
-            value_selected = 0
-        monthsnumber,xlabel,ylabel,title,color = self.get_value_params(value_selected)
-        xvalues,yvalues = self.get_values(values,value_selected,monthsnumber)
-
-        xval.append(xvalues)
-        yval.append(yvalues)
-        if value_selected2 < 0:
-            xlab.append("")
-        else:
-            xlab.append(xlabel)
-        ylab.append(ylabel)
-        tit.append(title)
-        col.append(color)
-
-        if value_selected2 < 0:
-            self.combovalue2.set_active(0)
-            value_selected2 = 0
-        if value_selected2 > 0:
-            value_selected2 = value_selected2-1
-            daysmonth,xlabel,ylabel,title,color = self.get_value_params(value_selected2)
-            xvalues,yvalues = self.get_values(values,value_selected2,daysmonth)
-            xval.append(xvalues)
-            yval.append(yvalues)
-            xlab.append(xlabel)
-            ylab.append(ylabel)
-            tit.append(title)
-            col.append(color)
-        self.drawarea.stadistics("bars",xval,yval,xlab,ylab,tit,col)
-
-    def get_values2(self,values,value_selected,monthsnumber):
-        #hacemos una relacion entre el value_selected y los values
-        conv = {
-            0: 1, #value 0 es kilometros (1)
-            1: 2, #value 1 es tiempo (2)
-            2: 3, #value 2 es pulsaciones(3)
-            3: 5, #value 3 es media(5)
-            4: 6 #value 4 es calorias(6)
-            }
-        list_values = {}
-        list_average = {}
-        tm_total = {}
-        for i in range(1,monthsnumber+1):
-            list_values[i]=0
-            list_average[i]=0
-            tm_total[i] = 0
-            
-        value_sel = conv[value_selected]
-
-        for value in values:
-            date = value[0]
-            try:
-                year,month,day = date.year, date.month, date.day
-            except AttributeError:
-                year,month,day = date.split("-")
-            month = int(month)
-            #si la opcion es tiempo lo pasamos a horas / if the option is time we passed it to hours
-            if (value_sel == 2):
-                graph_value = self.getFloatValue(value[value_sel])/3600
-            else:
-                graph_value = self.getFloatValue(value[value_sel])
-
-            #si es una opcion de suma de absolutos / if it is an option of sum of absolute
-            if ((value_selected == 0) or (value_selected==1) or (value_selected==4)):
-                list_values[int(month)] += graph_value
-            #si se trata de calcular medias / if one is to calculate averages:
-            else:
-                if graph_value is not None and graph_value != 0:
-                    list_values[int(month)] += graph_value
-                    list_average[int(month)] += 1
-
-        xunits = []
-        yunits = []
-        for i in range (0,monthsnumber):
-            xunits.append(locale_str(calendar.month_abbr[i+1]))
-            yunits.append(float(0))
-        for value in list_values:
-            if ((value_selected == 0) or (value_selected==1) or (value_selected==4)):
-                yunits[value-1] = list_values[value]
-            else:
-                if list_average[value]>0:
-                    yunits[value-1] = list_values[value]/list_average[value]
-        return xunits,yunits
-
-
