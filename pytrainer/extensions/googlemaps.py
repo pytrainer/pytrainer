pytrainer/pytrainer

commit 218b0603e8b8108ee8f7a8e3bfc25b9c9032ec16

Remove Python 2 fallback imports

pytrainer/extensions/googlemaps.py
@@ -23,10 +23,7 @@ import colorsys
 import math
 import traceback
 
-try:
-    import html
-except ImportError:
-    import cgi as html
+import cgi as html
 
 import pytrainer.lib.points as Points
 from pytrainer.lib.fileUtils import fileUtils

pytrainer/extensions/osm.py
@@ -9,10 +9,7 @@ import logging
 import requests         # for downloading cached versions of openlayers.js and openstreetmaps.js
 import time             # Used for checking if local cached file is current
 
-try:
-    import html
-except ImportError:
-    import cgi as html
+import cgi as html
 
 from pytrainer.lib.gpx import Gpx
 import pytrainer.lib.points as Points

pytrainer/lib/ddbb.py
@@ -105,15 +105,11 @@ if no url is provided"""
     def drop_tables(self):
         """Drop the database schema"""
         DeclarativeBase.metadata.drop_all(self.engine)
-                
+
     def create_backup(self):
         """Create a backup of the current database."""
-        try:
-            import urlparse
-            from urllib import url2pathname
-        except ImportError:
-            import urllib.parse as urlparse
-            from urllib.request import url2pathname
+        import urllib.parse as urlparse
+        from urllib.request import url2pathname
         scheme, netloc, path, params, query, fragment = urlparse.urlparse(self.url)
         if scheme == 'sqlite':
             import datetime

pytrainer/test/core/test_activity.py
@@ -16,10 +16,7 @@
 
 import unittest
 import datetime
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
+from unittest.mock import Mock
 from dateutil.tz import tzoffset
 from sqlalchemy.orm.exc import NoResultFound
 

pytrainer/test/gui/test_equipment.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
+from unittest.mock import Mock
 
 try:
     from gi.repository import Gtk

pytrainer/test/imports/test_garminfit.py
@@ -5,10 +5,7 @@ __copyright__ = "Copyright © 2013 David García Granda"
 __license__ = "GPL v2 or later"
 
 import unittest
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
+from unittest.mock import Mock
 import os
 import sys
 from lxml import etree

pytrainer/test/imports/test_garmintcxv2.py
@@ -5,10 +5,7 @@ __copyright__ = "Copyright © 2013 David García Granda"
 __license__ = "GPL v2 or later"
 
 import unittest
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
+from unittest.mock import Mock
 import os
 import sys
 from lxml import etree

pytrainer/test/lib/test_date.py
@@ -16,10 +16,7 @@
 
 import unittest
 import datetime
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
+from unittest.mock import Mock
 from dateutil.tz import tzutc, tzlocal
 from pytrainer.lib.date import second2time, time2second, time2string, unixtime2date, getNameMonth, getDateTime
 from pytrainer.lib.date import Date, DateRange

commit 53b4a6b282f651768b5a6f9d801c0a71f22d788e

Fix extensions html import
Broken in commit 218b0603e8b8 (which changed this in the wrong direction by
keeping the Python 2 fallback code and dropping the Python 3 version).
Thanks @hjerning for noticing so quickly.

pytrainer/extensions/googlemaps.py
@@ -16,18 +16,16 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-import os
-import re
-import logging
 import colorsys
+import html
+import logging
 import math
-import traceback
-
-import cgi as html
+import os
 
 import pytrainer.lib.points as Points
 from pytrainer.lib.fileUtils import fileUtils
 from pytrainer.lib.uc import UC
+
 
 class Googlemaps:
     def __init__(self, data_path = None, waypoint = None, pytrainer_main=None):

pytrainer/extensions/osm.py
@@ -3,19 +3,17 @@
 # TODO: Add Google satellite images layers ?
 # TODO: Create map class/interface that osm/googlemaps will inherit from ?
 
-import os
-import re
+import html
 import logging
-import requests         # for downloading cached versions of openlayers.js and openstreetmaps.js
-import time             # Used for checking if local cached file is current
+import os
+import time  # Used for checking if local cached file is current
 
-import cgi as html
+import requests  # for downloading cached versions of openlayers.js and openstreetmaps.js
 
-from pytrainer.lib.gpx import Gpx
 import pytrainer.lib.points as Points
 from pytrainer.lib.fileUtils import fileUtils
-from pytrainer.record import Record
 from pytrainer.lib.uc import UC
+
 
 class Osm:
     # Default URLS