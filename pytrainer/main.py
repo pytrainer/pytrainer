fb6debc1a1662f084a06f616bd9f64ee094971cc
Arto Jantunen <viiru@iki.fi>
2024-02-13 19:30:34 +0200

    Remove the "old" graph mode
    This hasn't worked in years, so no point in carrying the option and code.

--- pytrainer/main.py
+++ pytrainer/main.py
@@ -130,15 +130,13 @@ class pyTrainer:
         For more help on valid options try:
            %prog -h '''
         parser = OptionParser(usage=usage)
-        parser.set_defaults(log_level=logging.WARNING, validate=False, equip=False, newgraph=True, conf_dir=None, log_type="file")
+        parser.set_defaults(log_level=logging.WARNING, validate=False, equip=False, conf_dir=None, log_type="file")
         parser.add_option("-d", "--debug", action="store_const", const=logging.DEBUG, dest="log_level", help="enable logging at debug level")
         parser.add_option("-i", "--info", action="store_const", const=logging.INFO, dest="log_level", help="enable logging at info level")
         parser.add_option("-w", "--warn", action="store_const", const=logging.WARNING, dest="log_level", help="enable logging at warning level")
         parser.add_option("--error", action="store_const", const=logging.ERROR,
                           dest="log_level", help="enable logging at error level")
         parser.add_option("--valid", action="store_true", dest="validate", help="enable validation of files imported by plugins (details at info or debug logging level) - note plugin must support validation")
-        parser.add_option("--oldgraph", action="store_false", dest="newgraph", help="Turn off new graphing approach")
-        parser.add_option("--newgraph", action="store_true", dest="newgraph", help="Deprecated Option: Turn on new graphing approach")
         parser.add_option("--confdir", dest="conf_dir", help="Specify the directory where application configuration will be stored.")
         parser.add_option("--logtype", dest="log_type", metavar="TYPE",  type="choice" , choices=["file", "console"], help="Specify where logging should be output to. TYPE is one of 'file' (default), or 'console'.")
         (options, args) = parser.parse_args()
