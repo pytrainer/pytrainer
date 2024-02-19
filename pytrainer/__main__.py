2bb762c1bcedff41a3f33c1d3bd16a34380d9395
Arto Jantunen <viiru@iki.fi>
2024-02-19 20:42:50 +0200

    Parse options before initializing the main GUI class (pyTrainer)
    This should allow setting up logging _before_ localization, which allows
    using standard logging there instead of print().

--- pytrainer/__main__.py
+++ pytrainer/__main__.py
@@ -1,8 +1,98 @@
+import logging
+from optparse import OptionParser
+
+
+def get_options():
+    """
+    Define usage and accepted options for command line startup
+    """
+    usage = """usage: %prog [options]
+
+    For more help on valid options try:
+       %prog -h """
+    parser = OptionParser(usage=usage)
+    parser.set_defaults(
+        log_level=logging.WARNING,
+        validate=False,
+        equip=False,
+        newgraph=True,
+        conf_dir=None,
+        log_type="file",
+    )
+    parser.add_option(
+        "-d",
+        "--debug",
+        action="store_const",
+        const=logging.DEBUG,
+        dest="log_level",
+        help="enable logging at debug level",
+    )
+    parser.add_option(
+        "-i",
+        "--info",
+        action="store_const",
+        const=logging.INFO,
+        dest="log_level",
+        help="enable logging at info level",
+    )
+    parser.add_option(
+        "-w",
+        "--warn",
+        action="store_const",
+        const=logging.WARNING,
+        dest="log_level",
+        help="enable logging at warning level",
+    )
+    parser.add_option(
+        "--error",
+        action="store_const",
+        const=logging.ERROR,
+        dest="log_level",
+        help="enable logging at error level",
+    )
+    parser.add_option(
+        "--valid",
+        action="store_true",
+        dest="validate",
+        help="enable validation of files imported by plugins (details at info or debug logging level) - note plugin must support validation.",
+    )
+    parser.add_option(
+        "--oldgraph",
+        action="store_false",
+        dest="newgraph",
+        help="Turn off new graphing approach",
+    )
+    parser.add_option(
+        "--newgraph",
+        action="store_true",
+        dest="newgraph",
+        help="Deprecated Option: Turn on new graphing approach",
+    )
+    parser.add_option(
+        "--confdir",
+        dest="conf_dir",
+        help="Specify the directory where application configuration will be stored.",
+    )
+    parser.add_option(
+        "--logtype",
+        dest="log_type",
+        metavar="TYPE",
+        type="choice",
+        choices=["file", "console"],
+        help="Specify where logging should be output to. TYPE is one of 'file' (default), or 'console'.",
+    )
+    return parser.parse_args()
+
+
 def main():
+    options, args = get_options()
     import pytrainer.lib.localization
+
     pytrainer.lib.localization.initialize_gettext()
     from pytrainer.main import pyTrainer
-    pytrainer = pyTrainer()
+
+    pytrainer = pyTrainer(options)
+
 
 if __name__ == "__main__":
     main()
--- pytrainer/main.py
+++ pytrainer/main.py
@@ -19,7 +19,6 @@
 
 import sys
 import os
-from optparse import OptionParser
 import logging
 import logging.handlers
 import traceback
@@ -51,10 +50,9 @@ from .lib.uc import UC
 
 class pyTrainer:
 
-    def __init__(self):
+    def __init__(self, options):
         self.version = version
-        #Process command line options
-        self.startup_options = self.get_options()
+        self.startup_options = options
         #Setup logging
         self.environment = Environment(self.startup_options.conf_dir)
         self.environment.create_directories()
@@ -118,31 +116,6 @@ class pyTrainer:
         logging.debug('Launching main window...')
         self.windowmain.run()
         logging.debug('<<')
-
-    def get_options(self):
-        '''
-        Define usage and accepted options for command line startup
-
-        returns: options - dict with option: value pairs
-        '''
-        usage = '''usage: %prog [options]
-
-        For more help on valid options try:
-           %prog -h '''
-        parser = OptionParser(usage=usage)
-        parser.set_defaults(log_level=logging.WARNING, validate=False, equip=False, newgraph=True, conf_dir=None, log_type="file")
-        parser.add_option("-d", "--debug", action="store_const", const=logging.DEBUG, dest="log_level", help="enable logging at debug level")
-        parser.add_option("-i", "--info", action="store_const", const=logging.INFO, dest="log_level", help="enable logging at info level")
-        parser.add_option("-w", "--warn", action="store_const", const=logging.WARNING, dest="log_level", help="enable logging at warning level")
-        parser.add_option("--error", action="store_const", const=logging.ERROR,
-                          dest="log_level", help="enable logging at error level")
-        parser.add_option("--valid", action="store_true", dest="validate", help="enable validation of files imported by plugins (details at info or debug logging level) - note plugin must support validation.")
-        parser.add_option("--oldgraph", action="store_false", dest="newgraph", help="Turn off new graphing approach")
-        parser.add_option("--newgraph", action="store_true", dest="newgraph", help="Deprecated Option: Turn on new graphing approach")
-        parser.add_option("--confdir", dest="conf_dir", help="Specify the directory where application configuration will be stored.")
-        parser.add_option("--logtype", dest="log_type", metavar="TYPE",  type="choice" , choices=["file", "console"], help="Specify where logging should be output to. TYPE is one of 'file' (default), or 'console'.")
-        (options, args) = parser.parse_args()
-        return options
 
     def set_logging(self, level, log_type):
         '''Setup rotating log file with customized format'''
